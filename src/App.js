b51e814237f77f5efccfc908d85359059719860c
Noha-A-Thabet <NohaAshraf25@outlook.com>
2023-03-17 14:45:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [time, setTime] = useState(10);
+  const [displayButton, setDisplayButton] = useState(true)
+
+  const showNumber = () => {
+    setCounter(counter + 1)
+  }
+
+  useEffect(() => {
+
+    const intervalId = setInterval(() => {
+      if (time === 0) {
+        setDisplayButton(!displayButton)
+        return time;
+      }
+      setTime(() => time - 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [time])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>{counter}</div>
+      <h4>Time Left : {time} Seconds </h4>
+      {displayButton && <button onClick={showNumber} className="button">+</button>}
     </div>
   );
 }
